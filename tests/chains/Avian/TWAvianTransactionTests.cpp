f4234ec14d7cab9c5d03c2671e2d0f746988cafb
Craig Donnachie <craig.donnachie@victorinsurance.com>
2022-10-17 06:31:08 -0400

    Update AvianTransaction

--- tests/chains/Avian/TWAvianTransactionTests.cpp
+++ tests/chains/Avian/TWAvianTransactionTests.cpp
@@ -19,7 +19,7 @@
 
 namespace TW::Bitcoin {
 
-TEST(RavencoinTransaction, SignTransaction) {
+TEST(, SignTransaction) {
     /*
         https://iancoleman.io/bip39/
         Mnemonic - wink van interest where camera lens divert moon alter gentle what predict
